526bc455f4498a9886494bc3515cbb0f78adb4f6
Nara Lee <daisyleenr@gmail.com>
2019-05-09 19:38:28 +0900

    Fix travis.yml

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-eval "$(ssh-agent -s)"
-chmod 600 ./deploy_key
-echo -e "Host $SERVER_IP_ADDRESS\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-ssh-add ./deploy_key
-ssh -i ./deploy_key root@49.247.210.99 pwd
-docker-compose down
-docker-compose pull
-docker-compose up -d
